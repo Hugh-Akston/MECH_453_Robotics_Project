c17d554dad727d3715e0bf1f4dcbb1add9348cd7
Luke Freyhof <lmf92015@gmail.com>
2023-12-11 21:03:57 -0600

    Removed homework answers.

--- Path_Planning_Example/plot_line.m
+++
@@ -1,9 +0,0 @@
-%% Author Information
-% Written By: Dr. Eric Markvicka
-% Modified By: Luke Freyhof
-% Date: November 25, 2023
-
-function [] = plot_line(x,y)
-%% plot XY
-plot(x,y, 'r', 'linewidth', 2) % waypoints
-end
